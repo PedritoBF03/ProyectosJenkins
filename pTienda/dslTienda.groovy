71eca2891939c796346fd6173460c315473b6f53
PedritoBF03 <pedrobf2003@gmail.com>
2023-02-09 17:59:26 +0100

    actualizacion dslTienda

--- pTienda/dslTienda.groovy
+++ pTienda/dslTienda.groovy
@@ -1,20 +1,20 @@
 // import jenkins-auto-approve-script.groovy
 
 job('Job-pTienda') {
-    description('Job Padre para crear una tarea Hija que despliegue la App Tienda del Corte Ingles')
+    description('Job Padre para crear una tarea Hija que despliegue la App Tienda')
     scm {
-        git('https://github.com/morgadodesarrollador/jenkinsPTienda.git', 'master'){ 
-            node -> 
-                node / gitConfigName('morgadodesarrollador')
-                node / gitConfigEmail('morgadodesarrollador@gmail.com')
+        git('https://github.com/PedritoBF03/jenkinsPTienda.git', 'master') {
+            node ->
+                node / gitConfigName('PedritoBF03')
+                node / gitConfigEmail('pedrobf2003@gmail.com')
         }
     }
     triggers {
-     //   scm('H/5 * * * *')
+        //  scm('H/5 * * * *')
         githubPush()
     }
-    
+
     steps {
-        shell("bash install.sh")
+        shell('bash ./install.sh')
     }
 }
